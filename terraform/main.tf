80969bb41f51fdcb8c224884db18c4094b6e98e3
sewerynmi <seweryn.michota@digital.homeoffice.gov.uk>
2023-06-26 17:30:13 +0100

    more fun with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,49 @@ resource "github_repository" "example-repo" {
   name        = "example-repo"
   description = "My new repository for use with Terraform"
 }
+
+resource "github_team" "example-team" {
+  name        = "example-team"
+  description = "My new team for use with Terraform"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "example-team-membership" {
+  team_id  = "${github_team.example-team.id}"
+  username = "severinum"
+  role     = "member"
+}
+
+resource "github_team_membership" "example-team-membership" {
+  team_id  = "${github_team.example-team.id}"
+  username = "sewerynmi"
+  role     = "member"
+}
+
+resource "github_team_membership" "example-team-membership" {
+  team_id  = "${github_team.example-team.id}"
+  username = "SewerynMi01"
+  role     = "member"
+}
+
+# Add Team to Repo
+resource "github_team_repository" "example-team-repo" {
+  team_id    = "${github_team.example-team.id}"
+  repository = "${github_repository.example-repo.name}"
+  permission = "push"
+}
+
+variable "issue_labels" {
+  default = {
+    "custom-label"  = "533D99"
+    "documentation" = "FFB340"
+    "waiting-reply" = "CC6A14"
+  }
+}
+
+  resource "github_issue_label" "test_repo" {
+  repository = "${github_repository.example-repo.id}"
+  count      = "${length(var.issue_labels)}"
+  name       = "${element(keys(var.issue_labels), count.index)}"
+  color      = "${element(values(var.issue_labels), count.index)}"
+}
