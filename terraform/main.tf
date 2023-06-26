b3b258a22de23f11f52bddd0f72eb8bc5bfac0f4
sewerynmi <seweryn.michota@digital.homeoffice.gov.uk>
2023-06-26 19:36:59 +0100

    Add repo labels

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,72 +52,24 @@ resource "github_team_membership" "maintainers-team-membership-SewerynMi01" {
 }
 
 
-# resource "github_team_repository" "automation_repo_team" {
-#   team_id    = "${github_team.maintainers-team.id}"
-#   repository = "github-terraform-automation"
-#   permission = "push"
-# }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Create repository
-# resource "github_repository" "example-repo" {
-#   name        = "example-repo"
-#   description = "My new repository for use with Terraform"
-# }
-
-
-
-# Create example team
-# resource "github_team" "example-team" {
-#   name        = "example-team"
-#   description = "My new team for use with Terraform"
-#   privacy     = "closed"
-# }
-
-
-# Add user to the team
-# resource "github_team_membership" "example-team-membership" {
-#   team_id  = "${github_team.example-team.id}"
-#   username = "severinum"
-#   role     = "member"
-# }
-
-# Add Team to Repo
-# resource "github_team_repository" "example-team-repo" {
-#   team_id    = "${github_team.example-team.id}"
-#   repository = "${github_repository.example-repo.name}"
-#   permission = "push"
-# }
-
-
-# Add some isse labels colours
-# variable "issue_labels" {
-#   default = {
-#     "custom-label"  = "533D99"
-#     "documentation" = "FFB340"
-#     "waiting-reply" = "CC6A14"
-#   }
-# }
+#Add some issues labels colours
+variable "issue_labels" {
+  default = {
+    "task"  = "533D99"
+    "sub-task" = "FFB340"
+    "bugfix" = "CC6A14"
+  }
+}
 
 # Add issues labels
-#   resource "github_issue_label" "test_repo" {
-#   repository = "${github_repository.example-repo.id}"
-#   count      = "${length(var.issue_labels)}"
-#   name       = "${element(keys(var.issue_labels), count.index)}"
-#   color      = "${element(values(var.issue_labels), count.index)}"
-# }
+resource "github_issue_label" "issue_labels_repos" {
+  for_each   = module.repository
+  repository = each.value.name
+  count      = "${length(var.issue_labels)}"
+  name       = "${element(keys(var.issue_labels), count.index)}"
+  color      = "${element(values(var.issue_labels), count.index)}"
+}
