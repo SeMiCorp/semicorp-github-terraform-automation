3d8bc47f179909da4c3053f87bff8ebbbe4b29dd
sewerynmi <seweryn.michota@digital.homeoffice.gov.uk>
2023-06-26 17:40:17 +0100

    Comment out demo code

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,41 +7,52 @@ data "github_team" "semicorp_admins" {
   slug = "semicorpadmins"
 }
 
-resource "github_repository" "example-repo" {
-  name        = "example-repo"
-  description = "My new repository for use with Terraform"
-}
 
-resource "github_team" "example-team" {
-  name        = "example-team"
-  description = "My new team for use with Terraform"
-  privacy     = "closed"
-}
 
-resource "github_team_membership" "example-team-membership" {
-  team_id  = "${github_team.example-team.id}"
-  username = "severinum"
-  role     = "member"
-}
+# Create repository
+# resource "github_repository" "example-repo" {
+#   name        = "example-repo"
+#   description = "My new repository for use with Terraform"
+# }
+
+
+
+# Create example team
+# resource "github_team" "example-team" {
+#   name        = "example-team"
+#   description = "My new team for use with Terraform"
+#   privacy     = "closed"
+# }
+
+
+# Add user to the team
+# resource "github_team_membership" "example-team-membership" {
+#   team_id  = "${github_team.example-team.id}"
+#   username = "severinum"
+#   role     = "member"
+# }
 
 # Add Team to Repo
-resource "github_team_repository" "example-team-repo" {
-  team_id    = "${github_team.example-team.id}"
-  repository = "${github_repository.example-repo.name}"
-  permission = "push"
-}
+# resource "github_team_repository" "example-team-repo" {
+#   team_id    = "${github_team.example-team.id}"
+#   repository = "${github_repository.example-repo.name}"
+#   permission = "push"
+# }
 
-variable "issue_labels" {
-  default = {
-    "custom-label"  = "533D99"
-    "documentation" = "FFB340"
-    "waiting-reply" = "CC6A14"
-  }
-}
 
-  resource "github_issue_label" "test_repo" {
-  repository = "${github_repository.example-repo.id}"
-  count      = "${length(var.issue_labels)}"
-  name       = "${element(keys(var.issue_labels), count.index)}"
-  color      = "${element(values(var.issue_labels), count.index)}"
-}
+# Add some isse labels colours
+# variable "issue_labels" {
+#   default = {
+#     "custom-label"  = "533D99"
+#     "documentation" = "FFB340"
+#     "waiting-reply" = "CC6A14"
+#   }
+# }
+
+# Add issues labels
+#   resource "github_issue_label" "test_repo" {
+#   repository = "${github_repository.example-repo.id}"
+#   count      = "${length(var.issue_labels)}"
+#   name       = "${element(keys(var.issue_labels), count.index)}"
+#   color      = "${element(values(var.issue_labels), count.index)}"
+# }
