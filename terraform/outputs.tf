0bbcac94410d3ebb19b173e8c3ed344a6b1e4e49
sewerynmi <seweryn.michota@digital.homeoffice.gov.uk>
2023-06-26 18:46:21 +0100

    change name fo team

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,8 @@
 output "repositories" {
-  value = data.github_team.callisto.repositories
+  value = data.github_team.semicorp_admins.repositories
 }
 
 
 output "unconfigured_team_repositories" {
-  value = setsubtract(data.github_team.callisto.repositories, [for k, v in local.repository_configs : k])
+  value = setsubtract(data.github_team.semicorp_admins.repositories, [for k, v in local.repository_configs : k])
 }
