fe524afd96cd2ee23d43620e6a540d6b5534ec0d
sewerynmi <seweryn.michota@digital.homeoffice.gov.uk>
2023-06-27 20:08:13 +0100

    Add extra validation options

--- terraform/modules/repository/main.tf
+++ terraform/modules/repository/main.tf
@@ -10,12 +10,12 @@ resource "github_branch_protection_v3" "main" {
   }
   repository = data.github_repository.default.name
 
-  branch =                     "main"
+  branch = "main"
+  require_conversation_resolution = true
 
-  # required_status_checks {
-  #   strict   = true
-  #   checks = []
-  # }
+  required_status_checks {
+    strict   = true
+  }
 
   required_pull_request_reviews {
     dismiss_stale_reviews           = true
